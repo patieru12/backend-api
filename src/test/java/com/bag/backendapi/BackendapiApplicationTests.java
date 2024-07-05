2550fe357fe0faf367975a60fbbfdb7bcde33c12
Ruberandinda Patience <patience@gennxttechsol.com>
2024-07-05 15:04:40 +0200

    initial commit

---
+++ src/test/java/com/bag/backendapi/BackendapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bag.backendapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
